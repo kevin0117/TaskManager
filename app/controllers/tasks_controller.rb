b9a04495b3af80e2d6c262c745fe934f42cc5bcf
Kevin Wang <kevin0117@gmail.com>
2019-12-31 20:56:27 +0800

    完成 Task controller 基本CRUD 方法

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,55 @@
 class TasksController < ApplicationController
-end
+    before_action :find_task, only: [:edit, :show, :update, :destroy]
+  
+    def index
+      @tasks = Task.all
+    end
+  
+    def new
+      @task = Task.new
+    end
+  
+    def create
+      @task = Task.new(task_params)
+      if @task.save
+        redirect_to tasks_path, notice: "建立成功"
+      else
+        render :new, flash[:notice] = "建立失敗"
+        #render :new, notice: "建立失敗"  
+        #這裡的 render :new, notice: "建立失敗"
+        #其實是類似 render partial 的概念， 把 notice:"建立失敗" 當參數帶進去
+        #所以後面的 notice:"建立失敗 是沒有作用的
+      end
+    end
+  
+    def update
+      if @task.update(task_params)
+        redirect_to tasks_path, notice: "更新成功"
+      else
+        render :edit, flash[:notice] = "編輯失敗"
+      end
+    end
+  
+    def edit
+    end
+  
+    def show  
+    end
+  
+    def destroy
+      if @task.destroy
+        redirect_to tasks_path, notice: "刪除成功"
+      else
+        
+      end
+    end
+  
+    private
+    def find_task
+      @task = Task.find_by(id: params[:id])
+    end
+  
+    def task_params
+      params.require(:task).permit(:title, :content, :task_begin, :task_end, :priority, :status)
+    end
+  end
