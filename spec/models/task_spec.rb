935f82b12e8350604a854ecc03cb17c7f4dfa783
Kevin Wang <kevin0117@gmail.com>
2020-01-15 10:57:40 +0800

    更新單元測試 (model spec) 程式碼

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -4,99 +4,44 @@
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
-  describe "Model spec/單元測試" do 
-    it "任務建立成功當全部欄位正確填寫完成" do
-      task = Task.new(title: "Shopping", 
-                      content: "buy apple", 
-                      task_begin: "2018-11-01 09:00:00",
-                      task_end: "2019-11-01 00:00:00",
-                      priority: "urgent",
-                      status: "pending")
-      expect(task).to be_valid
+  describe 'Model spec/單元測試' do
+    context '如果任務建立成功時...' do
+      it '全部欄位正確填寫完成' do
+        task = FactoryBot.build(:task)
+        expect(task).to be_valid
+      end
+      it '狀態欄位可以是空白，因為預設是pending' do
+        task = FactoryBot.build(:task, status: '')
+        expect { expect(task).to be_valid }
+      end
+      it '任務開始日期不能是空白' do
+        task = FactoryBot.build(:task, task_begin: '')
+        expect { expect(task).to be_valid }.to raise_exception(/Task begin 不能為空白/)
+      end
+      it '任務結束日期不能是空白' do
+        task = FactoryBot.build(:task, task_end: '')
+        expect { expect(task).to be_valid }.to raise_exception(/Task end 不能為空白/)
+      end
+      it '任務優先順序不能是空白' do
+        task = FactoryBot.build(:task, priority: '')
+        expect{ expect(task).to be_valid }.to raise_exception(/Priority 不能為空白/)
+      end
+      it '名稱欄位不能是空白' do
+        task = FactoryBot.build(:task, title: '')
+        expect { expect(task).to be_valid }.to raise_exception(/Title 不能為空白/)
+      end
+      it '內容不能是空白' do
+        task = FactoryBot.build(:task, content: '')
+        expect { expect(task).to be_valid }.to raise_exception(/Content 不能為空白/)
+      end
     end
-
-    it "名稱欄位不能是空白，如果成功任務建立" do
-      task = Task.new(title: "", 
-                      content: "buy apple", 
-                      task_begin: "2019-11-01 09:00:00",
-                      task_end: "2019-11-01 00:00:00",
-                      priority: "urgent",
-                      status: "pending")
-        expect {
-          expect(task).to be_valid
-        }.to raise_exception(/Title 不能為空白/)
-    end
-    
-    it "內容不能是空白，如果成功任務建立" do
-      task = Task.new(title: "Shopping", 
-                      content: "", 
-                      task_begin: "2019-11-01 09:00:00",
-                      task_end: "2019-11-01 00:00:00",
-                      priority: "urgent",
-                      status: "pending")
-        expect {
-          expect(task).to be_valid
-        }.to raise_exception(/Content 不能為空白/)
-    end
-    
-    it "任務開始日期不能是空白，如果成功任務建立" do
-      task = Task.new(title: "Shopping", 
-                      content: "buy apple", 
-                      task_begin: "",
-                      task_end: "2019-11-01 00:00:00",
-                      priority: "urgent",
-                      status: "pending")
-        expect {
-          expect(task).to be_valid
-        }.to raise_exception(/Task begin 不能為空白/)
-    end
-    
-    it "任務結束日期不能是空白，如果成功任務建立" do
-      task = Task.new(title: "Shopping", 
-                      content: "buy apple", 
-                      task_begin: "2019-11-01 09:00:00",
-                      task_end: "",
-                      priority: "urgent",
-                      status: "pending")
-        expect {
-          expect(task).to be_valid
-        }.to raise_exception(/Task end 不能為空白/)
-    end
-    
-    it "任務優先順序不能是空白，如果成功任務建立" do
-      task = Task.new(title: "Shopping", 
-                      content: "buy apple", 
-                      task_begin: "2019-11-01 09:00:00",
-                      task_end: "2019-11-01 00:00:00",
-                      priority: "",
-                      status: "pending")
-        expect {
-          expect(task).to be_valid
-        }.to raise_exception(/Priority 不能為空白/)
-    end
-    # require'AASM'後, 預設狀態會是pending, 即使使用者未填寫，任務也是可以建立成功
-    it '任務狀態不能是空白，如果成功任務建立' do
-      task = Task.new(title: 'Shopping',
-                      content: 'buy apple',
-                      task_begin: '2019-10-01 09:00:00',
-                      task_end: '2019-11-01 00:00:00',
-                      priority: 'urgent',
-                      status: '')
-        expect {
-          expect(task).to be_valid
-        }
-    end 
-
-    it "任務建立失敗當結束時間設定錯誤" do
-      task = Task.new(title: "Shopping", 
-                      content: "buy apple", 
-                      task_begin: "Wed, 25 Nov 2019 19:06:00 CST +08:00",
-                      task_end: "Wed, 21 Nov 2019 19:06:00 CST +08:00",
-                      priority: "urgent",
-                      status: "pending")
-        expect {
-          expect(task).to be_valid
-        }.to raise_exception(/Task 任務結束日期不能比開始日期早/)
+    context '如果任務建立失敗...' do
+      it '當結束時間設定錯誤' do
+        task = FactoryBot.build(:task,
+                                task_begin: 'Wed, 25 Nov 2019 19:06:00',
+                                task_end: 'Wed, 21 Nov 2019 19:06:00')
+        expect { expect(task).to be_valid }.to raise_exception(/Task 任務結束日期不能比開始日期早/)
+      end
     end
   end
 end
