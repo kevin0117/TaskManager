45d443f89a1713b4d2f54eb0f04c756aa8d9b9b6
Kevin Wang <kevin0117@gmail.com>
2020-02-04 23:21:09 +0800

    更新 '使用者'& '任務' 樣板測試資料

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,6 +2,6 @@ FactoryBot.define do
   factory :user do
     username { Faker::Name.first_name }
     email { Faker::Internet.email }
-    id { Faker::Number.between(from: 1, to: 3) }
+    password { 'password' }
   end
 end
