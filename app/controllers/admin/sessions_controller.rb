ec8f2477e5b809057fff12d11f10f266a9a8730e
Kevin Wang <kevin0117@gmail.com>
2020-02-14 15:43:02 +0800

    新增後台 sessions controller 基本登入、登出方法

---
+++ app/controllers/admin/sessions_controller.rb
@@ -0,0 +1,31 @@
+class Admin::SessionsController < Admin::BaseController
+  before_action :find_session_user, only: %i[create delete]
+  def new; end
+
+  def create
+    if @user && @user.authenticate(params[:session][:password])
+      session[:user_id] = @user.id
+      flash[:notice] = "成功登入"
+      redirect_to task_admin_user_path(@user)
+    else
+      if (params[:session][:email] == '')
+        redirect_to admin_login_path, notice: 'Email 不可空白'
+      elsif (params[:session][:password] == '')
+        redirect_to admin_login_path, notice: '密碼 不可空白'
+      else
+        redirect_to admin_login_path, notice: '使用者資料輸入錯誤'
+      end
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to admin_login_path, notice: '已登出'
+  end
+
+  private
+
+  def find_session_user
+    @user = User.find_by(email: params[:session][:email].downcase)
+  end
+end
