f773dbe8b1169fbcc605fa71cefb4c6edf44d169
Kevin Wang <kevin0117@gmail.com>
2020-02-16 17:38:08 +0800

    只有管理員可以登入後台，一般使用者存取管理頁面時，將被提示權限不足訊息無法存取，並轉向適當頁面

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,10 +1,18 @@
 class Admin::BaseController < ApplicationController
   layout 'backend'
+  before_action :login_check
+  before_action :admin_check
 
   # 進入任務管理頁面必須先登入
   def login_check
     if !logged_in?
       redirect_to admin_login_path, notice: '請先登入'
+    end
+  end
+  # 登入使用者必須是管理員角色
+  def admin_check
+    if !current_user.admin
+      redirect_to admin_login_path, notice: '權限不足'
     end
   end
 end
