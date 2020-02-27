8fd4b8806edb4acabefee1427331c5a15d715123
Kevin Wang <kevin0117@gmail.com>
2020-02-28 03:18:25 +0800

    新增標籤超連結呈現方法

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -1,2 +1,8 @@
 module TasksHelper
+  def display_tag(tag_names)
+    tag_names.map { |name|
+      "<span class='badge badge-pill badge-light'>
+      #{link_to name, tag_path(name)}
+      </span>"}.join(' ').html_safe
+  end
 end
