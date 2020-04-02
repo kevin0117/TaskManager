91c1c8eb4f1b091886f057c4d931847ac634abc5
Kevin Wang <kevin0117@gmail.com>
2020-04-02 20:03:00 +0800

    建立 ActonMailer 所需設定

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,6 +62,19 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.delivery_method = :production
+  config.action_mailer.default_url_options = { host: 'https://taskmanager0117.herokuapp.com/' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: ENV['smtp_address'],
+    port: 587,
+    domain: 'postmaster@chienhao.tw',
+    user_name: ENV['smtp_username'],
+    password: ENV['smtp_password'],
+    authentication: 'plain',
+    enable_starttls_auto: true
+  }
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
