3d80789dcc72f5340017144c3792a8a378254ec7
Kevin Wang <kevin0117@gmail.com>
2020-04-03 23:25:49 +0800

    程式碼更新：讓先前 sidekiq 所設定的 MailWorker handle background jobs

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,7 +18,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      UserMailer.with(user: @user).welcome_email.deliver_later
+      MailWorker.perform_async(@user.id)
       redirect_to root_path, notice: "#{@user.username} 已註冊成功！"
     else
       @error_message = @user.errors.full_messages.to_sentence
